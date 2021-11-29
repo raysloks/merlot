e77e0e14e2b296c5a418784863b6d6846fe04776
Emil <raysloks@gmail.com>
2021-11-29 20:57:57 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,15 +14,16 @@ $(window).resize(sectionHeight);
 
 $(function () {
 	$("section h1, section h2, section h3").each(function () {
-		let parent = "nav ul:first-child";
+		let parent = "nav";
 		console.log($(this).text().toLowerCase());
 		for (let i = 1; "h" + i !== this.nodeName.toLowerCase(); ++i) {
-			parent += " .tag-h" + i + ":last-child ul:last-child";
+			parent += " .tag-h" + i + ":last-child";
 		}
+		parent += " > ul";
 		console.log(parent);
 		$(parent).append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '') + "'>" + $(this).text() + "</a><ul></ul></li>");
 		$(this).attr("id", $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, ''));
-		$("nav ul:first-child li:first-child a").parent().addClass("active");
+		$("nav > ul li:first-child a").parent().addClass("active");
 	});
 
 	$("nav ul li").on("click", "a", function (event) {
